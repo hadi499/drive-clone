9d34085510327a0b0fa36c38a1e799cd6a63a163
hadi499 <hadi.ygcl@gmail.com>
2024-07-08 05:58:38 +0700

    make path and render files

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\File;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
+use App\Http\Resources\FileResource;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreFolderRequest;
 
@@ -12,7 +13,17 @@ class FileController extends Controller
 {
     public function myFiles()
     {
-        return Inertia::render('MyFiles');
+        $folder = $this->getRoot();
+        $query = File::query()
+            ->select('files.*')           
+            ->where('created_by', Auth::id())
+            ->where('_lft', '!=', 1)
+            ->orderBy('is_folder', 'desc')
+            ->orderBy('files.created_at', 'desc')
+            ->orderBy('files.id', 'desc');
+        $files = $query->paginate(10);
+        $files = FileResource::collection($files);
+        return Inertia::render('MyFiles', compact('files', 'folder'));
     }
 
     public function createFolder(StoreFolderRequest $request)
